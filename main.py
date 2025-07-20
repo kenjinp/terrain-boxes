02088f4b6bf00ddde6f47192ce74434c5ad31d8f
Nick Sanford <nicholascsanford@gmail.com>
2025-07-20 18:58:17 -0400

    wip

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from PIL import Image
+import numpy as np
+import sys
+
+
+def main():
+    if len(sys.argv) <= 1:
+        print(f"usage: {sys.argv[0]} <image.tiff>")
+        sys.exit(1)
+    file = sys.argv[1]
+    no_ext = "./"+ ''.join(file.split(".")[:-1])
+    im = Image.open(file)
+    a = np.array(im)
+    int16 = a.astype('int16')
+    int16.tofile(no_ext+".raw")
+    Image.fromarray(int16).save(no_ext+".png")
+
+if __name__ == "__main__":
+    main()
