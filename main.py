a920ba1c1d872ba6056f38a2ce209cc55879afc9
Nick Sanford <nicholascsanford@gmail.com>
2025-07-20 19:17:11 -0400

    wip

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
         sys.exit(1)
     file = sys.argv[1]
     no_ext = "./"+ ''.join(file.split(".")[:-1])
-    im = Image.open(file)
+    im = Image.open(file).resize((2048, 2048))
     a = np.array(im)
     int16 = a.astype('int16')
     int16.tofile(no_ext+".raw")
